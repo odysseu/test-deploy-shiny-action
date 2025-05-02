5c88501c3dcc9a86f17ccdb366387959ef4067d8
Ulysse Boucherie <ulysse.boucherie.interieur.gouv.fr>
2025-05-02 12:22:22 +0200

    Add simple R shiny app

---
+++ populations/server.R
@@ -0,0 +1,29 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        seed <- 1234
+        x    <- rand(min = 2016, max = 2025, 100)
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Years',
+             main = 'Random population sizes')
+
+    })
+
+}
